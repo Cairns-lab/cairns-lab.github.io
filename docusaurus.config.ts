b05c33aed75df63f7482250e06a594e4e3884df7
alex <alex@dhcp-10-3-117-44.newcastle.edu.au>
2025-10-16 11:04:30 +1100

    reset

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -9,21 +9,23 @@ const config: Config = {
   tagline: 'The Centre for Complex Disease and Precision Medicine',
   favicon: 'img/CL_logo.png',
 
+  deploymentBranch: 'gh-pages',
+
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
     v4: true, // Improve compatibility with the upcoming Docusaurus v4
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://cairns-lab.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'cairns-lab', // Usually your GitHub org/user name.
+  projectName: 'cairns-lab.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
